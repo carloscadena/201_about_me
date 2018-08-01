d1a5d7f58cb64eeb4dcd70df02cc768ea432204d
Carlos Cadena <cs.cadena@gmail.com>
2018-07-31 17:06:44 -0700

    first quiz question working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+'use strict';
+
 console.log('js is linked');
+
+var userName = prompt('What is your name?');
+var quizQuestion = confirm('Hi ' + userName + '! Would you like to take a quiz?');
+if (quizQuestion){
+  runQuiz();
+} else {
+  alert('No worries ' + userName + '. Welcome to my page!');
+}
+
+function runQuiz(){
+  alert('Great! This quiz will be five questions but if you get any wrong the quiz is over. Can you make it through all five? Please answer all questions with either a yes or a no, or a y or n. Capitalization doesn\'t matter. I\'m very sophisticated');
+  var quizOne = prompt('Do I have a pet?');
+  if(quizOne.toLowerCase() === 'no' || quizOne.toLowerCase() === 'n'){
+    alert('That is correct, unfortunately.');
+  } else {
+    alert('Incorrect! I\'ll have a pet someday. Feel free to play again but in the mean time, welcome to my page!');
+  }
+
+
+
+}
